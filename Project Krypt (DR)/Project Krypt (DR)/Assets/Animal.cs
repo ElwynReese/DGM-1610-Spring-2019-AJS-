c99940249b74b1ad5a0f7b69bc43ea0a83ed49ab
D <ronin.taiho@gmail.com>
2019-02-04 16:01:51 -0700

    Build 000 - Animal Classes Intro #0

--- Project Krypt (DR)/Project Krypt (DR)/Assets/Animal.cs
+++ Project Krypt (DR)/Project Krypt (DR)/Assets/Animal.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 
 public class Animal : MonoBehaviour
 {
-
 	public string Name;
+	public string Identity;
+	public int powerLevel;
+	public int topSpeed;
+	public int agility;
+	public bool flying;
+	public bool swimming;
+	public int diet;
+	public int weight;
+	public bool fur;
+	public bool exoskeleton;
 
 	public Color SkinColor;
 	// Use this for initialization
--- Project Krypt (DR)/Project Krypt (DR)/Assets/Fish.cs
+++ Project Krypt (DR)/Project Krypt (DR)/Assets/Fish.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Boo.Lang;
 using UnityEngine;
 
 public class Fish : Animal
 {
-
+	
 	public Color FinColor;
 	// Use this for initialization
 	void Start () {
