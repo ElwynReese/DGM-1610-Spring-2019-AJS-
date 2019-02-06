00e183f021f16c429440a74b4a93a1ab704b0ea7
D <ronin.taiho@gmail.com>
2019-02-05 17:03:37 -0700

    Build 001 - Animal Classes Intro #1

---
+++ Project Krypt (DR)/Project Krypt (DR)/Assets/AnimalKingdom/Rodent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rodent : Animal
+{
+	public bool isSmall = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
