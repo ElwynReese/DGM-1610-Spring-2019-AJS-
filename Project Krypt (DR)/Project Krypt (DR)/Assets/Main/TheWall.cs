e8aabb49ceab6d52e454d9f6745ad6019187362d
D <ronin.taiho@gmail.com>
2019-02-06 20:47:57 -0700

    Build 002 - Structure

---
+++ Project Krypt (DR)/Project Krypt (DR)/Assets/Main/TheWall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Think of this class like a job board, a collection of tasks for passive processes to take care of... WIP
+public class TheWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
