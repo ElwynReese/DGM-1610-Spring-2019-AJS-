08774152a1ca89d6861917b97e1b8d531b25eb18
D <ronin.taiho@gmail.com>
2019-02-07 18:19:55 -0700

    Build 003 - Structure 1

---
+++ Project Krypt (DR)/Project Krypt (DR)/Assets/Main/AiGlobal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This will be the proxy between implicit and explicit AI functions. It will also serve to translate existing class functions
+//within unity into unique AI behaviours.
+
+public class AiGlobal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Project Krypt (DR)/Project Krypt (DR)/Assets/Main/TheWall.cs
+++ Project Krypt (DR)/Project Krypt (DR)/Assets/Main/TheWall.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //Think of this class like a job board, a collection of tasks for passive processes to take care of... WIP
+//This also serves as the proxy diagnostic class, it will report conflicts an issues with associated classes.
 public class TheWall : MonoBehaviour {
 
 	// Use this for initialization
