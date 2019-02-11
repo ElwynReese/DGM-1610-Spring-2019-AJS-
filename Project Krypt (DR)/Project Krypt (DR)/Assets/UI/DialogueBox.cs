0bca9e990ddcc9b5c86871630424701195b617f8
D <ronin.taiho@gmail.com>
2019-02-10 23:04:51 -0700

    Build 005 - Structure 3

---
+++ Project Krypt (DR)/Project Krypt (DR)/Assets/UI/DialogueBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This is an iteration of an idea involving on screen dialogue display with character portraits. More later.
+
+public class DialogueBox : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
