11a04d6a166f1066b87674b2098f87f7a2f871d4
D <ronin.taiho@gmail.com>
2019-02-11 09:50:50 -0700

    Build 007 - Structure 5

---
+++ Project Krypt (DR)/Project Krypt (DR)/Assets/Main/PCCEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PCCEvent : MonoBehaviour
+{
+public UnityEvent Event;
+
+private void OnCollisionEnter()
+{
+    Event.Invoke();
+    Destroy(GameObject.FindWithTag("Player"));
+}
+}
