4cde1c5e873876de94764bec2267f5d3082706ac
D <ronin.taiho@gmail.com>
2019-02-12 22:45:54 -0700

    Build 008 - Structure 6

---
+++ Project Krypt (DR)/Project Krypt (DR)/Assets/Opposition/EnemyLogicDef.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This is a placeholder for a comparable defensive Enemy AI Logic Script. Will fill it in later.
+
+public class EnemyLogicDef : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
