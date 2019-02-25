5ca4401f054f3c33dc3f29d6285fbbc721f64cd5
D <ronin.taiho@gmail.com>
2019-02-25 11:34:03 -0700

    Build 018 - Class Feb 25, If-Else Statements

---
+++ Project Krypt (DR)/Project Krypt (DR)/Assets/Main/Conditionals.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Conditionals : MonoBehaviour
+{
+    public bool CanWalk;
+    public int Number = 11;
+    public string Password;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CanWalk)
+        {
+            print("True");
+        }
+        else
+        {
+            print(false);
+        }
+
+        if (Number >= 10)
+        {
+            print("Greater");
+        }
+        else
+        {
+            print("Lesser");
+        }
+//The below statement is an if-else efficiency condensation produced by JetBrains.
+        print(Password == "OU812" ? "The password is correct." : "The Password is incorrect.");
+    }
+}
---
+++ Project Krypt (DR)/Project Krypt (DR)/Assets/Main/LightSwitchController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.Events;
+
+
+public class LightSwitchController : MonoBehaviour
+{
+    public UnityEvent Event;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Event.Invoke();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
