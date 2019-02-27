e2b8829c9cfd77c6b7e5367860c6494c8188c6ec
D <ronin.taiho@gmail.com>
2019-02-26 23:54:11 -0700

    Build 019 - GUI (Cursor Systems)

--- Project Krypt (DR)/Project Krypt (DR)/Assets/Main/MouseController.cs
+++ Project Krypt (DR)/Project Krypt (DR)/Assets/Main/MouseController.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.UIElements;
 
 public class MouseController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public LayerMask clickableObject;
 
+    public Texture2D pointer;
+
+    public Texture2D target;
+
+    public Texture2D combat;
+
+    public Texture2D doorway;
+    
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit Hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out Hit, 50, clickableObject.value))
+        {
+            bool door = false;
+            if (Hit.collider.gameObject.tag == "Doorway")
+            {
+                Cursor.SetCursor(doorway, new Vector2(16, 16), CursorMode.Auto);
+                door = true;
+            }
+            else
+            {
+                Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
+            }
+        }
+        else
+        {
+            Cursor.SetCursor(pointer, Vector2.zero, CursorMode.Auto);
+        }
     }
 }
