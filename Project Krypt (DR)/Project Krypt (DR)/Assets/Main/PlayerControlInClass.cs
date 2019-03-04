94328aae8c3f9cd6cd4d18eec89c7b633799c3ce
D <ronin.taiho@gmail.com>
2019-03-04 11:56:14 -0700

    Build 019d - Class Mar 4 + Misc (Character Control)

--- Project Krypt (DR)/Project Krypt (DR)/Assets/Main/PlayerControlInClass.cs
+++ Project Krypt (DR)/Project Krypt (DR)/Assets/Main/PlayerControlInClass.cs
@@ -9,11 +9,14 @@ public class PlayerControlInClass : MonoBehaviour
 
 	public UnityEvent OnGrounded;
 	public UnityEvent OffGrounded;
-	public float Speed = 3;
+
+//	public float Speed = 5f;
+//	public float Gravity = -9.81f;
+//	public float JumpForce = 2f;
 	
 	private CharacterController controller; //This didn't have to be public as it no longer needs to be accessed in the editor.
 
-	private Vector3 position; //Its generally best if as many variables as possible are private.
+//	private Vector3 position; //Its generally best if as many variables as possible are private.
 	
 	// Use this for initialization
 	void Start ()
@@ -33,9 +36,24 @@ public class PlayerControlInClass : MonoBehaviour
 		{
 		 OffGrounded.Invoke();
 		}
-		position.y = Input.GetAxis("Vertical")*Speed*Time.deltaTime;
-		position.x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+
+//		if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0)
+//		{
+//			position.y = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+//		}
+//		else
+//		{
+//			position.y = Gravity * Time.deltaTime;	
+//		}
+//		if (controller.isGrounded && Input.GetButton("Jump"))
+//		{
+//			position.y = JumpForce;  //This doesn't work quite right.
+//		}
 		
-		controller.Move(position); //This will put the ship in the 0,0,0 position.
+//		position.y += Gravity * Time.deltaTime;
+		
+//		position.x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+		
+//		controller.Move(position); //This will put the ship in the 0,0,0 position. This is what is actually making it move...
 	}
 }
